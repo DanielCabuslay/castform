3fdbdec908c98a3f8ec1a01b79ad273f45eab5aa
Daniel Cabuslay <ddccabuslay@gmail.com>
2020-06-04 16:33:53 -0400

    Reverted file location references

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue'
-import App from '@/App.vue'
-import '@/registerServiceWorker'
-import store from '@/store'
+import App from './App.vue'
+import './registerServiceWorker'
+import store from './store'
 import VueResource from 'vue-resource'
 
 Vue.config.productionTip = false
