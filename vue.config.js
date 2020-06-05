b8bb0ad8fe4266a4ac7c43783c2b2b570c948453
Daniel Cabuslay <ddccabuslay@gmail.com>
2020-06-05 10:17:28 -0400

    Updated assets for PWA and manifest

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  pwa: {
+    name: 'Castform',
+    themeColor: '#2680D9',
+    msTileColor: '#2680D9',
+    appleMobileWebAppCapable: 'no',
+    appleMobileWebAppStatusBarStyle: 'default'
+  }
+}
